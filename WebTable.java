6136d0dba8344f2a316d6f92555dc771890da598
kowsalya3696 <60561292+kowsalya3696@users.noreply.github.com>
2021-09-20 23:35:29 -0700

    Add files via upload

--- WebTable.java
+++ WebTable.java
@@ -1,5 +1,6 @@
 package Week3Assessment;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
@@ -9,7 +10,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
-public class WebTable{
+public class WebTable {
 
 public static void main(String[] args) {
 	WebDriverManager.chromedriver().setup();
@@ -17,21 +18,19 @@ public static void main(String[] args) {
 	driver.get("https://www.w3schools.com/html/html_tables.asp");
 	driver.manage().window().maximize();
     driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-	List<WebElement> allRows = driver.findElements(By.xpath("//table//tbody//th"));
-	for (int i = 0; i <3; i++) {
-		System.out.println(allRows.get(i).getText()+" ");
-	}
-	List<WebElement> allcells = driver.findElements(By.xpath("//table//tbody//td"));
-	for (int i = 0; i < 18; i++) {
-		System.out.println(allcells.get(i).getText()+" ");
+    
+    List<WebElement> allRows = driver.findElements(By.xpath("//table[@id='customers']//tr"));
+    int rowSize = allRows.size(); 
+    //System.out.println(rowSize);
+    for (int i = 1; i <= rowSize; i++) {
+    List<WebElement> allcells = driver.findElementsByXPath("//table[@id='customers']//tr["+i+"]/td");
+	int cellcount  = allcells.size();
+	//System.out.println(cellcount);
+    for(int j = 1; j <=cellcount; j++) {
+	String text = driver.findElementByXPath("//table[@id='customers']//tr["+i+"]/td["+j+"]").getText();
+	System.out.println(text);
+		
+    }
+    }}
+}
 
-	}
-	
- }
- }
-
-
-
-  
-  
- 
--- flipkart.java
+++ flipkart.java
@@ -1,7 +1,7 @@
 package Week3Assessment;
 
+import java.util.Set;
 import java.util.concurrent.TimeUnit;
-
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
@@ -10,16 +10,36 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class flipkart {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		WebDriverManager.chromedriver().setup();
 		ChromeDriver driver = new ChromeDriver();
 		driver.get("https://www.flipkart.com/");
 		driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        driver.findElementByXPath("//button[@class ='_2KpZ6l _2doB4z']").click();
         WebElement Fashion = driver.findElementByXPath("//div[@class='eFQ30H'][4]");
         Actions builder = new Actions(driver);
         builder.moveToElement(Fashion).perform();
-        
-      	}
-
-}
+        driver.findElementByLinkText("Men's Bottom Wear").click();
+        Thread.sleep(5000);
+        WebElement source = driver.findElementByXPath("//div[@class='HQL4QS _28DFQy']");
+        builder.dragAndDropBy(source, 101, 0).build().perform();
+        Thread.sleep(5000);
+        WebElement source1 = driver.findElementByXPath("//div[@class='HQL4QS WC_zGJ']");
+        builder.dragAndDropBy(source1, -68, 0).build().perform();
+        Thread.sleep(5000);
+        String parentWindow = driver.getWindowHandle();
+     	driver.findElementByXPath("//span[contains(text(), 'Showing')]/parent::div/following::a").click();
+     	Set<String> allWindows = driver.getWindowHandles();
+     	java.util.Iterator<String> iterator = allWindows.iterator();			
+     	while (iterator.hasNext()) {
+     	String childWindow = iterator.next();
+     	if (!parentWindow.equalsIgnoreCase(childWindow)) {
+     	driver.switchTo().window(childWindow);
+     	String retrunPolicyText = driver.findElementByXPath("//div[contains(text(),'Return Policy')]").getText();
+     	String days = retrunPolicyText.replaceAll("[^0-9]", " ").trim();
+     	System.out.println(" Return Policy days :" + days);
+     				}
+     			}
+     		}}
+     	
